2c01587410669a1fba3cb9742bae57825e29b4e8
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2020-07-12 16:48:01 +0530

    Added Authentication

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,11 +1,19 @@
+const jwt = require("jsonwebtoken");
+
 const db = require("../models");
 
 exports.register = async (req, res, next) => {
   try {
     const user = await db.User.create(req.body);
     const { id, username } = user;
-    res.json({ id, username });
+
+    const token = jwt.sign({ id, username }, process.env.SECRET);
+
+    res.status(201).json({ id, username, token });
   } catch (error) {
+    if (error.code === 11000) {
+      error.message = "Sorry, that username is already taken.";
+    }
     next(error);
   }
 };
@@ -14,16 +22,19 @@ exports.login = async (req, res, next) => {
   try {
     const user = await db.User.findOne({ username: req.body.username });
     if (user == null) {
-      throw new Error("Invalid Username/Password");
+      throw new Error();
     }
     const { id, username } = user;
     const valid = await user.comparePassword(req.body.password);
     if (valid) {
-      res.json({ id, username });
+      const token = jwt.sign({ id, username }, process.env.SECRET);
+
+      res.status(201).json({ id, username, token });
     } else {
-      throw new Error("Invalid Username/Password");
+      throw new Error();
     }
   } catch (error) {
+    error.message = "Invalid Username/Password";
     next(error);
   }
 };
